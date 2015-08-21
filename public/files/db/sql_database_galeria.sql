45df035a35794c3fa97b5c003839f3c9fb843b66
nandorodpires2 <contato@newfinances.com.br>
2015-08-21 19:43:08 -0300

    database

--- public/files/db/sql_database_galeria.sql
+++ public/files/db/sql_database_galeria.sql
@@ -50,15 +50,16 @@ CREATE TABLE IF NOT EXISTS `exposicao` (
   KEY `fk_exposicao_usuario1_idx` (`usuario_id`),
   CONSTRAINT `fk_exposicao_tipo_exposicao` FOREIGN KEY (`tipo_exposicao_id`) REFERENCES `tipo_exposicao` (`tipo_exposicao_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_exposicao_usuario1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`usuario_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
--- Copiando dados para a tabela galeria.exposicao: ~3 rows (aproximadamente)
+-- Copiando dados para a tabela galeria.exposicao: ~5 rows (aproximadamente)
 /*!40000 ALTER TABLE `exposicao` DISABLE KEYS */;
 INSERT INTO `exposicao` (`exposicao_id`, `tipo_exposicao_id`, `exposicao_nome`, `exposicao_descricao`, `exposicao_capa`, `exposicao_data_criacao`, `exposicao_ativo`, `usuario_id`) VALUES
 	(2, 2, 'Conversíveis Jornal Super', 'Miniaturas de conversíveis da década de 60.', 'capa_galeria_1.jpg', '2015-08-19 14:14:23', 0, 1),
 	(4, 1, 'Obras de Monet', 'As belíssimas obras do consagrado pintor renascentista. Obras dos séculos XII e XIII.', 'monet.jpg', '2015-08-19 17:21:19', 1, 2),
 	(5, 6, 'Lindas Mulheres', 'As mais belas mulheres do site belladasemana. Uma gata mais sexy do que a outra. Confira!', 'capa.jpg', '2015-08-20 14:33:10', 1, 1),
-	(6, 6, 'Animais fofinhos', 'Coleção de fotos de animais em belos ângulos para você se apaixonar...', 'Cuddling-Huskies.jpg', '2015-08-20 19:32:08', 1, 1);
+	(6, 6, 'Animais fofinhos', 'Coleção de fotos de animais em belos ângulos para você se apaixonar...', 'Cuddling-Huskies.jpg', '2015-08-20 19:32:08', 1, 1),
+	(7, 6, 'Lindas Paisagens', 'As mais belas paisagens das montanhas de Minhas.', 'Bela-Imagem.jpg', '2015-08-21 12:05:06', 1, 1);
 /*!40000 ALTER TABLE `exposicao` ENABLE KEYS */;
 
 
@@ -94,7 +95,7 @@ CREATE TABLE IF NOT EXISTS `exposicao_vizualizacao` (
   CONSTRAINT `fk_exposicao_vizualizacao_exposicao1` FOREIGN KEY (`exposicao_id`, `usuario_id`) REFERENCES `exposicao` (`exposicao_id`, `usuario_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 
--- Copiando dados para a tabela galeria.exposicao_vizualizacao: ~17 rows (aproximadamente)
+-- Copiando dados para a tabela galeria.exposicao_vizualizacao: ~19 rows (aproximadamente)
 /*!40000 ALTER TABLE `exposicao_vizualizacao` DISABLE KEYS */;
 INSERT INTO `exposicao_vizualizacao` (`exposicao_vizualizacao_id`, `exposicao_vizualizacao_data`, `exposicao_id`, `usuario_id`) VALUES
 	(1, '2015-08-20 14:00:13', 2, 1),
